0f467e6f3174bc224c0c59925a06f8d57690ef8f
Amir <amirdragon0808@gmail.com>
2019-04-05 17:49:47 +0600

    Indexator

---
+++ IndexatorHW/IndexatorHW/MyArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace IndexatorHW
+{
+    public class MyArray
+    {
+        public int[] array;
+
+        public MyArray(int size)
+        {
+            array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set { array[index] = Convert.ToInt32(Math.Pow(array[index], 2)); }
+        }
+        public void Print()
+        {
+            Console.WriteLine("Array:");
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ IndexatorHW/IndexatorHW/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace IndexatorHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize;
+            int insertValue;
+            Console.WriteLine("Enter Size of Array");
+            int.TryParse(Console.ReadLine(), out arraySize);
+            MyArray massiv = new MyArray(arraySize);
+            Console.WriteLine("Enter Insert of Value");
+            int.TryParse(Console.ReadLine(), out insertValue);
+            massiv[insertValue] = insertValue;
+
+
+            Console.WriteLine("Changed Element Value= " + massiv[insertValue]);
+            Console.WriteLine("Changed Element Index " + insertValue);
+            massiv.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
