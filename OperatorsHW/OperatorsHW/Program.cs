b2cb48c3e3f133a093debb0889ab00377c893323
Amir <amirdragon0808@gmail.com>
2019-04-06 17:12:59 +0600

    operatorsHW

---
+++ OperatorsHW/OperatorsHW/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TankLib.WorldOfTanks;
+
+namespace OperatorsHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int amountOfTanks = 5;
+
+            string pantera = "Pantera";
+            string t34 = "T-34";
+
+            Tank[] tanks34 = new Tank[amountOfTanks];
+            Tank[] tanksPantera = new Tank[amountOfTanks];
+            Console.WriteLine("\nT-34 Tanks: \n");
+            for (int i = 0; i < tanks34.Length; i++)
+            {
+                tanks34[i] = new Tank(t34);
+                tanks34[i].Print();
+            }
+
+            Console.WriteLine("\nPantera Tanks: \n");
+            for (int i = 0; i < tanks34.Length; i++)
+            {
+                tanksPantera[i] = new Tank(pantera);
+                tanksPantera[i].Print();
+            }
+            Console.WriteLine("\nFights: \n");
+
+
+            for (int i = 0; i < amountOfTanks; i++)
+            {
+                string Game = tanks34[i] * tanksPantera[i];
+                Console.WriteLine(Game);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperatorsHW/TankLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace TankLib.WorldOfTanks
+{
+    public class Tank
+    {
+        private string _tankName;
+        private int _ammunition;
+        private int _armor;
+        private int _agility;
+
+        public Tank(string tankName)
+        {
+            Thread.Sleep(500);
+            Random random = new Random();
+            _tankName = tankName;
+            _ammunition = random.Next(0, 100);
+            _armor = random.Next(0, 100);
+            _agility = random.Next(0, 100);
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name       : {_tankName}\nAmmunition : {_ammunition}\nArmor      : {_armor}\nAgility    : {_agility}");
+            Console.WriteLine("--------------------");
+        }
+
+        public static string operator *(Tank firstTank, Tank secondTank)
+        {
+            int Counter = 0;
+            try
+            {
+                if (firstTank._tankName == null || secondTank._tankName == null)
+                    throw new Exception("Названия танков не заданы!");
+                else
+                {
+                    if (firstTank._ammunition > secondTank._ammunition)
+                    {
+                        Counter++;
+                    }
+                    if (firstTank._armor > secondTank._armor)
+                    {
+                        Counter++;
+                    }
+                    if (firstTank._agility > secondTank._agility)
+                    {
+                        Counter++;
+                    }
+                    if (Counter > 1) { return "T-34 win!\n"; }
+                    else { return "Pantera win!\n"; }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Исключение: {exception.Message}");
+                return "Танк не найден!\n";
+            }
+        }
+    }
+}
