0fe9cc879ba5e4b40e5ea5c8a2b349895f1c493f
Amir <amirdragon0808@gmail.com>
2019-04-14 16:36:03 +0600

    OnlineBD1HW

---
+++ OnlineBD1HW/OnlineBD1HW/Program.cs
@@ -0,0 +1,18 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBD1HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tableGruppa = new DBservice();
+            tableGruppa.AddTable();
+        }
+    }
+}
---
+++ OnlineBD1HW/Services/DBservice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Common;
+//using System.Data.SqlClient;
+
+namespace Services
+{
+    public class DBservice
+    {
+        private readonly string _connectionstring;
+        private readonly string _providerName;
+        private readonly DbProviderFactory _dbProviderFactory;
+        private DbTransaction transaction;
+        public DBservice()
+        {
+            _connectionstring = ConfigurationManager.ConnectionStrings["testconnectionString"].ConnectionString;
+            _providerName = ConfigurationManager.ConnectionStrings["testconnectionString"].ProviderName;
+            _dbProviderFactory = DbProviderFactories.GetFactory(_providerName);
+        }
+
+        public void AddTable()
+        {
+            using (var connection = _dbProviderFactory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                try
+                {
+                    connection.ConnectionString = _connectionstring;
+                    connection.Open();
+                    transaction = connection.BeginTransaction();
+                    command.CommandText = $"use Database;" +
+                        $"go;" +
+                        $"create table Gruppa(" +
+                        $"id int not null identity(1,1) primary key," +
+                        $"Name nvarchar(256) not null";
+                    command.Transaction = transaction;
+
+                    transaction.Commit();
+                    transaction.Dispose();
+                }
+                catch (DbException exception)
+                {
+                    transaction?.Rollback();
+                    transaction?.Dispose();
+                    throw;
+                }
+                catch (Exception exception)
+                {
+                    transaction?.Rollback();
+                    transaction?.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
+
