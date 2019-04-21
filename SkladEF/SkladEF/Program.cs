af0ad38986d0a893125d68050613275efc474159
Amir <amirdragon0808@gmail.com>
2019-04-21 22:55:17 +0600

    SkladEFHW

---
+++ SkladEF/SkladEF/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkladEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add tovar");
+            Tovar tovar = new Tovar();
+            Console.WriteLine("Enter name tovar");
+            tovar.Name = Console.ReadLine();
+            Console.WriteLine("Enter price of tovar");
+            tovar.Price = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter amount of tovar");
+            tovar.Amount = int.Parse(Console.ReadLine());
+            using (var context = new SkladContext())
+            {
+                context.Tovars.Add(tovar);
+                context.SaveChanges();
+            }
+            Console.WriteLine("Enter name tovar for find it");
+            Tovar findTovar = new Tovar();
+            findTovar.Id = Guid.Empty;
+            findTovar.Name = Console.ReadLine();
+            List<Tovar> tovars = new List<Tovar>();
+            using (var context = new SkladContext())
+            {
+                tovars = context.Tovars.ToList();
+            }
+            foreach (Tovar t in tovars)
+            {
+                if (t.Name.Equals(findTovar.Name) == true)
+                {
+                    findTovar.Id = t.Id;
+                }
+            }
+            if (findTovar.Id != Guid.Empty)
+            {
+                Console.WriteLine("Enter price of tovar");
+                findTovar.Price = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter amount of tovar");
+                findTovar.Amount = int.Parse(Console.ReadLine());
+            }
+            else Console.WriteLine("Tovar dont exist");
+            using (var context = new SkladContext())
+            {
+                var findTovars = context.Tovars;
+                foreach (Tovar t in findTovars)
+                {
+                    if (t.Id == findTovar.Id)
+                    {
+                        t.Price = findTovar.Price;
+                        t.Amount = findTovar.Amount;
+                    }
+                }
+                context.SaveChanges();
+            }
+            Console.WriteLine("For delete tovar enter his name");
+            string deleteName = Console.ReadLine();
+            using (var context = new SkladContext())
+            {
+                var deleted = context.Tovars;
+                Tovar deleteTovar;
+                foreach (Tovar t in deleted)
+                {
+                    if (t.Name.Equals(deleteName)==true)
+                    {
+                        deleteTovar = t;
+                        context.Tovars.Remove(deleteTovar);
+                    }
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SkladEF/SkladEF/SkladContext.cs
@@ -0,0 +1,31 @@
+﻿namespace SkladEF
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class SkladContext : DbContext
+    {
+        // Контекст настроен для использования строки подключения "SkladContext" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "SkladEF.SkladContext" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "SkladContext" 
+        // в файле конфигурации приложения.
+        public SkladContext()
+            : base("name=SkladContext")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+        public DbSet<Tovar> Tovars { get; set; }
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
