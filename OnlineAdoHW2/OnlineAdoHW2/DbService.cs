8c89f6d5a0aa443fce66600beb932b974013f139
Amir <amirdragon0808@gmail.com>
2019-04-28 21:48:40 +0600

    OnlineAdoHW2

---
+++ OnlineAdoHW2/OnlineAdoHW2/DbService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace OnlineAdoHW2
+{
+    public class DBservice
+    {
+        private readonly string _providerName;
+        private SqlConnection sqlConnection;
+        private SqlTransaction transaction;
+        public SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+        public DBservice()
+        {
+            stringBuilder.ApplicationName = "OnlineAdoHW2";
+            stringBuilder.AttachDBFilename = @"C:\Users\Dell\source\repos\OnlineAdoHW2\OnlineAdoHW2\Database.mdf";
+            stringBuilder.DataSource = @"(LocalDB)\MSSQLLocalDB";
+            stringBuilder.IntegratedSecurity = false;
+        }
+
+        public void AddTable(string connectionstring)
+        {
+            using (var connection = new SqlConnection())
+            using (var command = connection.CreateCommand())
+            {
+                try
+                {
+                    connection.ConnectionString = connectionstring;
+                    connection.Open();
+                    transaction = connection.BeginTransaction();
+                    command.CommandText = $"use Database;" +
+                        $"go;" +
+                        $"create table Gruppa(" +
+                        $"id int not null identity(1,1) primary key," +
+                        $"Name nvarchar(256) not null";
+                    command.Transaction = transaction;
+
+                    transaction.Commit();
+                    transaction.Dispose();
+                }
+                catch (DbException exception)
+                {
+                    transaction?.Rollback();
+                    transaction?.Dispose();
+                    throw;
+                }
+                catch (Exception exception)
+                {
+                    transaction?.Rollback();
+                    transaction?.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ OnlineAdoHW2/OnlineAdoHW2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnlineAdoHW2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private string _connectionstring;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string _login, _password;
+            _login = userLogin.Text.ToString();
+            _password = userPassword.Text.ToString();
+            DBservice dbService = new DBservice();
+            if (string.IsNullOrWhiteSpace(_login) == false&& string.IsNullOrWhiteSpace(_password) == false)
+            {
+                dbService.stringBuilder.UserID = _login;
+                dbService.stringBuilder.Password = _password;
+                _connectionstring = dbService.stringBuilder.ConnectionString;
+                dbService.AddTable(_connectionstring);
+            }
+        }
+    }
+}
